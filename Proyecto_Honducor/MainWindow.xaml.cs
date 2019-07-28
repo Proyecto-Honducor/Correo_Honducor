5f66f82cb81013f25bca98ac5c4c16d12f5b4518
marioargueta150@gmail.com <marioargueta150@gmail.com>
2019-07-27 18:35:22 -0600

    menu principal

--- Proyecto_Honducor/MainWindow.xaml.cs
+++ Proyecto_Honducor/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Proyecto_Honducor
 {
@@ -23,6 +24,116 @@ namespace Proyecto_Honducor
         public MainWindow()
         {
             InitializeComponent();
+            startclock();
+        }
+
+        private void startclock()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += tickevent;
+            timer.Start();
+        }
+
+        private void tickevent(object sender, EventArgs e)
+        {
+            datelbl.Text = DateTime.Now.ToLongTimeString();
+            hourlbl.Text = DateTime.Now.ToLongDateString();
+        }
+
+        private void ButtonAbrirMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCerrarMenu.Visibility = Visibility.Visible;
+            ButtonAbrirMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonCerrarMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCerrarMenu.Visibility = Visibility.Collapsed;
+            ButtonAbrirMenu.Visibility = Visibility.Visible;
+        }
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UserControl usc = null;
+
+            switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
+            {
+                case "ItemEmpleados":
+                    //usc = new UserControlEmpleados();
+                    //GridMain.Children.Add(usc);
+                    break;
+                case "ItemFacturacion":
+                    //usc = new UserControlFacturacion();
+                    //GridMain.Children.Add(usc);
+                    break;
+                case "ItemHistorial":
+                   // usc = new UserControlHistorial();
+                    //GridMain.Children.Add(usc);
+                    break;
+                case "ItemCargo":
+                    //usc = new UserControlCarg();
+                    //GridMain.Children.Add(usc);
+                    break;
+                case "ItemCategoria":
+                    //usc = new UserControlCategoria();
+                    //GridMain.Children.Add(usc);
+                    break;
+                case "ItemClientes":
+                    //usc = new UserControlClientes();
+                    //GridMain.Children.Add(usc);
+                    break;
+                case "ItemProducto":
+                    //usc = new UserControlProducto();
+                   // GridMain.Children.Add(usc);
+                    break;
+                case "ItemUsuarios":
+                    //usc = new UserControlUsuarios();
+                    //ridMain.Children.Add(usc);
+                    break;
+                default:
+                    break;
+            }
+        }
+        //Login login = new Login();
+        private void BtnSalir_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (MessageBox.Show("Realmente desea salir?", "Consulta", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                App.Current.Shutdown();
+            }
+            else
+            {
+                //No hace nada
+            }
+        }
+
+        private void BtnCerrarSesion_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Realmente desea cerrar sesion?", "Consulta", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+              //  login.Show();
+                this.Close();
+            }
+            else
+            {
+                //No hace nada
+            }
+
+        }
+
+        private void BtnAyuda_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnCuenta_Click(object sender, RoutedEventArgs e)
+        {
+
+            UserControl usc = null;
+          //  usc = new Ayuda();
+            //GridMain.Children.Add(usc);
+
         }
     }
 }
